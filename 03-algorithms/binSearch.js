53cfbecd2f22ef5b1bd53f31fba970cd5ca8b286
David Linkowski <david.linkowski@gmail.com>
2026-02-06 14:21:01 -0700

    Added binSearch solution and lru-cache solution to algorithms

---
+++ 03-algorithms/binSearch.js
@@ -0,0 +1,34 @@
+/*
+Create a function that takes a sorted array and a target value. 
+Return the index of the target value in the array. 
+If the target value is not in the array, return -1.
+*/
+
+function binSearch(arr, val) {
+  if (arr === undefined || arr === null) return -1;
+  const n = arr.length;
+  if (n === 0) return -1;
+  let hi = n - 1;
+  let lo = 0;
+  let mid;
+
+  while (lo <= hi) {
+    mid = lo + Math.floor((hi - lo) / 2);
+    if (val < arr[mid]) {
+      hi = mid - 1;
+    } else if (val > arr[mid]) {
+      lo = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+  return -1;
+}
+
+function main() {
+  const arr = [1, 2, 3, 5, 6];
+  let rslt = binSearch(arr, 5);
+  console.log(rslt);
+}
+
+main();
---
+++ 03-algorithms/lru-cache.js
@@ -0,0 +1,50 @@
+/*
+LRU Interview Question
+Create a data structure that implements the requirements of a Least Recently Used (LRU) cache with O(1) average time complexity.
+
+Initialize an object with a maxium capacity of elements.
+getItem Return the value of the key. Update cache with the most recently used key.
+putItem Create or update a key value pair in the cache. Evict the least recently used key if the size of keys exceeds the max capacity.
+*/
+
+function LRU(cap) {
+  this.capacity = cap;
+  this.cache = new Map(); // key -> value, insertion order = recency
+}
+
+LRU.prototype.getItem = function (key) {
+  if (!this.cache.has(key)) return null;
+  const val = this.cache.get(key); // delete
+  this.cache.set(key, val); // and reset to front
+  return val;
+};
+
+LRU.prototype.putItem = function (key, val) {
+  if (this.capacity <= 0) return;
+
+  if (this.cache.has(key)) this.cache.delete(key);
+
+  this.cache.set(key, val);
+
+  if (this.cache.size > this.capacity) {
+    const lruKey = this.cache.keys().next().value;
+    this.cache.delete(lruKey);
+  }
+};
+
+function main() {
+  const cache = new LRU(5);
+  cache.putItem("a", 1);
+  console.log(cache.cache);
+  cache.putItem("a", 2);
+  console.log(cache.cache);
+  cache.putItem("b", 3);
+  cache.putItem("c", 4);
+  cache.putItem("d", 5);
+  cache.putItem("e", 6);
+  console.log(cache.cache);
+  cache.putItem("f", 7);
+  console.log(cache.cache);
+}
+
+main();
